f82c81eff25866b8c44b43dbd4e1984066bd2134
simoZak <zakhnini92@gmail.com>
2024-05-01 23:03:27 +0100

    task 0 bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,57 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-int swapped = 0; // global variable to check if swap() function is called
-void swap(int *a, int *b)
-{
-    int temp = *b;
-    *b = *a;
-    *a = temp;
-    swapped++;
-}
-int bubblesort(int *a, int size) // to demonstrate passing by refference in C (pointer variable receives only the base address of array)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-            swap(a+j, a+j+1); // making a function is good instead of many lines of code in main function 
-            }
-        }
-        if (swapped == 0)
-            return 1; // use return values better than break statement
-    }
-    return 0; // sorted 
+#include "sort.h"
 
-}
-
-int main()
+/**
+ * bubble_sort - sort list with bubble
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
 {
-    int n, i;
-    int *a = NULL; // security issue 
-    printf("Please insert the number of elements to be sorted: ");
-    do
-    {
-        scanf("%d", &n); // The total number of elements
-    } while(n > 21); // never trust the user , heap-overflow , max elements 20 here
+	size_t n = 0, t = size;
+	int holder;
 
-    a = (int *)malloc(n * sizeof(int)); // Note: It would be better to use malloc over calloc, unless we want the zero-initialization because malloc is faster than calloc. So if we just want to copy some stuff or do something that doesn’t require filling of the blocks with zeros, then malloc would be a better choice.
-    for ( i = 0; i < n; i++)
-    {
-        printf("Input element %d :", i);
-        scanf("%d", (a+i)); // Adding the elements to the array
-    }
-    printf("unsorted list: "); // Displaying the unsorted array
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    bubblesort(a, n);
-    printf("\nSorted list:\n"); // Display the sorted array
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", *(a+i));// we're using C , so demonstrate pointers :) 
-    }
-    return 0;
+	if (array == NULL)
+		return;
+	for (t = size; t > 0; t--)
+	{
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				holder = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = holder;
+				print_array(array, size);
+			}
+		}
+	}
 }
